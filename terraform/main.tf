2d4230d2e55e06c72defe9efaa8df56601fca33d
Tatenda Prince Moyo <tatendamoyo539@gmail.com>
2025-04-30 19:58:41 +0200

    updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,11 @@
+# Create the SQS queue
 resource "aws_sqs_queue" "ci_events_queue" {
-  name                      = "ci-events-queue"
+  name                       = "ci-events-queue"
   visibility_timeout_seconds = 30
   message_retention_seconds  = 86400
 }
 
+# Create the Lambda function
 resource "aws_lambda_function" "event_processor" {
   function_name = "sqs-event-processor"
   role          = aws_iam_role.lambda_exec_role.arn
@@ -12,20 +14,28 @@ resource "aws_lambda_function" "event_processor" {
   timeout       = 10
   memory_size   = 128
 
+  # Lambda deployment package
   filename         = "${path.module}/lambda.zip"
   source_code_hash = filebase64sha256("${path.module}/lambda.zip")
 
+  # Environment variables (e.g., for Slack notifications)
   environment {
     variables = {
       SLACK_WEBHOOK_URL = var.slack_webhook_url
     }
   }
+
+  # Optional: You can add tags to help identify resources
+  tags = {
+    Name        = "SQS Event Processor"
+    Environment = "dev"
+  }
 }
 
-
+# Map SQS queue to Lambda function
 resource "aws_lambda_event_source_mapping" "sqs_to_lambda" {
-  event_source_arn  = aws_sqs_queue.ci_events_queue.arn
-  function_name     = aws_lambda_function.event_processor.arn
-  batch_size        = 5
-  enabled           = true
+  event_source_arn = aws_sqs_queue.ci_events_queue.arn
+  function_name    = aws_lambda_function.event_processor.arn
+  batch_size       = 5
+  enabled          = true
 }
